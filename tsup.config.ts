b3e52d83c4143bd63db00a9cdb7401ec13251682
Abhishek Kumar Majumdar <dev.abhishekmajumdar@gmail.com>
2026-09-09 15:46:07 +0530

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -54,21 +54,4 @@ export default defineConfig([
     outDir: 'dist',
     outExtension: () => ({ js: '.js' }),
   },
-]);import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: {
-    index: 'src/index.ts',
-    'cache/index': 'src/cache/index.ts',
-    'logger/index': 'src/logger/index.ts',
-    'retry/index': 'src/retry/index.ts',
-  },
-  format: ['cjs', 'esm'],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  treeshake: true,
-  minify: false,
-  outDir: 'dist',
-});
+]);
